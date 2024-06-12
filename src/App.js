2bc60ad49d2e3a9b32c60262d6d7a1e50ea016ab
jinithajohnson <jinithajohnson02@gmail.com>
2024-06-12 23:24:22 +0530

    success

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Add from './components/Add';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Search from './components/Search';
+import View from './components/view';
+import ViewAll from './components/ViewAll';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
         <Route path='/' element={<Add/>} />
-        <Route path='/' element={<Search/>} />
+        <Route path='/search' element={<Search/>} />
+        <Route path='/view' element={<ViewAll/>} />
+        
         
 
     </Routes>
